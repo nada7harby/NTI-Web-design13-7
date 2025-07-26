4f14230a145d252edce820154e90f33f2296ba77
nada7harby <87335429+nada7harby@users.noreply.github.com>
2025-07-26 14:22:21 +0300

    day 10

---
+++ day 10/script.js
@@ -0,0 +1,90 @@
+// var name = "nada55555";
+// console.log(name);
+// var age = 22;
+// console.log(age);
+// console.log(55);
+// console.log(" hi   nadaa");
+
+// alert("hiii"+name)
+// var name = prompt("Enter your name");
+// alert(name);
+
+// var city = "sohag";
+// console.log(city);
+// alert("hello");
+
+// let age=prompt();
+
+// var name=prompt("Enter your Name");
+
+// alert("hello "+name)
+
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 % 2);
+
+// var num1 = 15;
+// var num2 = 20;
+// var result = num1 * num2;
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 == 2);
+
+// if (5 > 2) {
+//   console.log("bigger1");
+// }
+// if (5 < 2) {
+//   console.log("bigger2");
+// } else {
+//   console.log("smaller");
+// }
+
+// if (5 == 2) {
+//   console.log("bigger3");
+// }
+
+// var num1=prompt("enter first num")
+// var num2=prompt("enter second num")
+// if (num1 == num2) {
+//   console.log("they are Ecal");
+//     var city="sohag"
+// } else if (num1 > num2) {
+//   console.log("they are big");
+// } else {
+//   console.log("they are small");
+// }
+
+// console.log("ali")
+// console.log("ali")
+// console.log("ali")
+// console.log("ali")
+// console.log("ali")
+// console.log("ali")
+// console.log("ali")
+// console.log("ali")
+// console.log("ali")
+// console.log("ali")
+
+// var count = 1;
+// while (count < 5) {
+//   console.log("ali");
+//   console.log("nada"); // 1 2 3
+//   count = count + 1; // 2 3 4 5
+// }
+
+
+
+function sayhi() {
+  var name = prompt();
+  alert("hi " + name);
+  var count = 1;
+//   while (count < 5) {
+//     console.log(count);
+//     count = count + 1; // 2 3 4 5
+//   }
+
+for (let i = 2; i < 3; i++) {
+  console.log(i); //1  1 1
+}
+}
